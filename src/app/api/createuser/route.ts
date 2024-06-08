4c4aa08e6c8e8c4adc124157ef973edfdd81d1d4
Adwaith Raj <adwaithraj45654@gmail.com>
2024-06-09 00:14:17 +0530

    apis

--- src/app/api/createuser/route.ts
+++ src/app/api/createuser/route.ts
@@ -1,13 +1,14 @@
 // pages/api/users.js
 //@ts-ignore
-import { NextRequest } from 'next/server';
-import clientPromise from '../../../lib/mongodb';
+import { NextRequest } from "next/server";
+//@ts-ignore
+import clientPromise from "../../../lib/mongodb";
 //@ts-ignore
 // export default async function handler(req, res) {
 //     //@ts-ignore
 //   const client = await clientPromise;
 //   const db = client.db('users'); // use your database name
- 
+
 //   switch (req.method) {
 //     case 'GET':
 //         console.log('in get')
@@ -24,50 +25,49 @@ import clientPromise from '../../../lib/mongodb';
 //       break;
 //   }
 // }
-export async function POST(req:NextRequest)
-{
-  const cookies=req.cookies.get("authCookie")
-  console.log(`all cookies are ${cookies}`)
-    //@ts-ignore
-    const client = await clientPromise;
-    const db = client.db('data'); // use your database name
-  console.log('in post')
- 
-  const { name, email,IEEEID,isLead,team } = await req.json();
+export async function POST(req: NextRequest) {
+  const cookies = req.cookies.get("authCookie");
+  console.log(`all cookies are ${cookies}`);
+  //@ts-ignore
+  const client = await clientPromise;
+  const db = client.db("data"); // use your database name
+  console.log("in post");
+
+  const { name, email, IEEEID, isLead, team } = await req.json();
   const newUser = {
     name,
     email,
     IEEEID,
     isLead,
-    team
+    team,
   };
-  const existingUser = await db.collection('users').findOne({ email:email });
+  const existingUser = await db.collection("users").findOne({ email: email });
   if (existingUser) {
     return Response.json({
-      message:"email already exists",
-      user:existingUser
-    })
+      message: "email already exists",
+      user: existingUser,
+    });
   }
 
-  if(isLead && !IEEEID)
-  return  Response.json({
-    message:"create failed. Team lead has no IEEE id"
-  })
-
-  console.log(`new user is ${name} ${email} `)
-  
-  if(!name || !email || !team)
+  if (isLead && !IEEEID)
     return Response.json({
-        message:'error!! required fields name, email and team not found'
-    })
+      message: "create failed. Team lead has no IEEE id",
+    });
 
-  const users = await db.collection('users').insertOne(newUser);
- 
+  console.log(`new user is ${name} ${email} `);
+
+  if (!name || !email || !team)
+    return Response.json({
+      message: "error!! required fields name, email and team not found",
+    });
+
+  const users = await db.collection("users").insertOne(newUser);
+
   // res.json(users);
   //@ts-ignore
   return Response.json({
-    message:"user added successfully",
-    user:users,
-    cookies:cookies
-  })
+    message: "user added successfully",
+    user: users,
+    cookies: cookies,
+  });
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 import Dash from "@/components/ui/dash";
 import Navbar from "@/components/ui/navbar";
 import Register from "@/components/ui/register";
 import React, { useState } from "react";
-import {useEffect} from 'react'
+import { useEffect } from "react";
 
 export default function Dashboard() {
-  const [email,setEmail]=useState('')
-useEffect(()=>{
-  const email=localStorage.getItem('email')
-  setEmail(email)
-  console.log(`current email is ${email}`)
-},[])
+  const [email, setEmail] = useState<string | null>("");
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    setEmail(email);
+    console.log(`current email is ${email}`);
+  }, []);
   return (
-    
     <div className="text-white">
       <Navbar></Navbar>
-      <Register></Register>
-      <Dash></Dash>
+      <Register userEmail={email}></Register>
+      <Dash userEmail={email}></Dash>
     </div>
   );
 }
