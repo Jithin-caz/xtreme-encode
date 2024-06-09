bbd6dd74222fc5ea13563d33041b9793c108d58f
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-06-09 23:26:41 +0530

    new

--- src/app/finalRegister/page.tsx
+++ src/app/finalRegister/page.tsx
@@ -67,13 +67,13 @@ export default function FinalRegistration()
         getTeam()
     },[])
 
-    return !loading?<section className=" bg-slate-950 p-10 py-8 pt-16 h-dvh text-purple-800">
+    return !loading?<section className=" bg-slate-950 p-10 py-8 pt-16 min-h-dvh text-indigo-700">
         { isRegistered?
-            <h1 className=" text-center  text-4xl font-semibold  text-purple-700">Registration complete</h1>:
-            <h1 className=" text-2xl  text-purple-700 font-semibold">Once registered changes cannot be reverted!! <br />If you wish to edit
+            <h1 className=" text-center  text-4xl font-semibold   text-indigo-700">Registration complete</h1>:
+            <h1 className=" text-2xl  text-purple-900 font-semibold">Once registered changes cannot be reverted!! <br />If you wish to edit
             , go back to the previous page</h1>}
 
-            <h1  className=" pt-14 text-white text-xl"><span className="opacity-85 text-purple-700">teamname:&nbsp;</span>{teamname}</h1>
+            <h1  className=" pt-14 text-white text-xl"><span className="opacity-85  text-indigo-700">teamname:&nbsp;</span>{teamname}</h1>
                
                 <div className=" mt-11 w-full grid grid-cols-1 lg:grid-cols-3 gap-5 ">
                     {
@@ -82,13 +82,13 @@ export default function FinalRegistration()
                         //@ts-ignore
                         key={member} className=" bg-slate-900 border-2 border-slate-800 p-8 col-span-1 rounded-lg shadow-xl">
                            
-                            <div className=" w-full text-white font-sans flex items-center text-lg pt-3 border-t-2 border-purple-700"><span className=" opacity-85"> name: &nbsp;</span>  {member.name}
+                            <div className=" w-full text-white font-sans flex items-center text-lg pt-3 border-t-2 border-indigo-700"><span className=" opacity-85"> name: &nbsp;</span>  {member.name}
                               {index==0 &&<div className=" text-white font-sans text-sm pt-3">(lead)</div>}</div>
                             <div className=" w-full text-white font-sans text-lg pt-3"><span className=" opacity-85">email:</span> {member.email}</div>
                             <div className=" w-full text-white font-sans text-lg pt-3"><span className=" opacity-85">ieeid:</span> {member.ieeeId===""?"null":member.ieeeId}</div>
                             <div className=" w-full text-white font-sans text-lg pt-3"><span className=" opacity-85">branch:</span> {member.branch}</div>
                             <div className=" w-full text-white font-sans text-lg pt-3"><span className=" opacity-85">college:</span> {member.college}</div>
-                            <div className=" w-full text-white font-sans text-lg pt-3 pb-4 border-b-2 border-purple-700"><span className=" opacity-85">country:</span> {member.country}</div>
+                            <div className=" w-full text-white font-sans text-lg pt-3 pb-4 border-b-2 border-indigo-700"><span className=" opacity-85">country:</span> {member.country}</div>
                         </div>)
                     }
                 </div>
