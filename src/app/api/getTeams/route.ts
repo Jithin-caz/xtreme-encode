a2d215bf3e0e914374115b2537dd2648e14e8791
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-08-01 20:47:08 +0530

    new

---
+++ src/app/api/getTeams/route.ts
@@ -0,0 +1,28 @@
+//@ts-ignore
+import clientPromise from "@/lib/mongodb";
+
+export async function GET(req:any, res:any)
+{
+ 
+  //@ts-ignore
+  const client = await clientPromise;
+  const db = client.db('data');
+  const teams = await db.collection('teams')
+  .find({ registered: true, score: { $ne: 0 }  })
+  .sort({ score: -1 }) 
+  .toArray();
+ 
+//console.log(teams)
+  return new Response(
+    JSON.stringify({
+      message: "the teams",
+      teams
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
---
+++ src/app/leaderboards/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { async } from "@firebase/util"
+import axios from "axios"
+import { useEffect,useState } from "react"
+import Loading from "../loading"
+
+export default function LeaderBoard(){
+    const [teams, setTeams]=useState([])
+    const getTeamScore=async()=>{
+        await axios.get('/api/getTeams').then((response)=>{
+            console.log(response.data.teams)
+            setTeams(response.data.teams)
+           
+           
+        })
+    }
+    useEffect(()=>{
+        getTeamScore()
+    },[])
+    return teams.length>0?<section className=" overflow-clip min-h-dvh bg-gradient-to-br items-center from-slate-900 to-slate-800 flex flex-col p-3">
+        <h1 className=" font-mono text-blue-50 text-4xl pt-7">Leaderboard</h1>
+        <p className=" text-yellow-300 opacity-80 text-sm">team with same score are given same preference despite the different ranks</p>
+        <div className=" w-full flex justify-center px-2">
+        <table className=" mt-11 bg-slate-950 rounded-lg">
+            <thead className=" sticky top-0 z-50 bg-white rounded-lg bg-opacity-90 shadow-inner backdrop-blur-sm">
+                <tr>
+                    <th className="px-5 py-3 text-slate-950 text-xl">Rank</th>
+                    <th className="px-5 py-3 text-slate-950 text-xl">Teamname</th>
+                    <th className="px-5 py-3 text-slate-950 text-xl">Score</th>
+                </tr>
+            </thead>
+            <tbody className=" max-h-dvh overflow-scroll">
+            {teams.map((team,index)=><tr key={index}>
+                <td  className="px-5 py-3 text-white text-center">
+                    {index+1}
+                </td>
+                <td  className="px-5 py-3 text-white text-center">
+                    {
+                     //@ts-ignore
+                    team.team}
+                </td>
+                <td  className="px-5 py-3 text-white text-center">
+                {
+                //@ts-ignore
+                team.score}
+                </td>
+            </tr>)}
+           
+            </tbody>
+        </table></div>
+    </section>:<Loading/>
+}
